b7acaf750940b638862072f31627d806cc1ec7d0
CamerasDecoded <camerasdecoded@gmail.com>
2026-08-19 00:29:07 -0500

    Add files via upload

--- js/firebase-init.js
+++ js/firebase-init.js
@@ -1,8 +1,4 @@
 // js/firebase-init.js
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js';
-import { getAuth } from 'https://www.gstatic.com/firebasejs/10.12.0/firebase-auth-compat.js';
-import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore-compat.js';
-
 const firebaseConfig = {
   apiKey: "AIzaSyB95Vx0i8W6WNfUy1N4TNQyfN5xCxQYnz8",
   authDomain: "cameras-decoded.firebaseapp.com",
@@ -13,6 +9,8 @@ const firebaseConfig = {
   measurementId: "G-YN3M01WW0B"
 };
 
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+// Initialize using the global firebase object
+firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+export const db = firebase.firestore();
